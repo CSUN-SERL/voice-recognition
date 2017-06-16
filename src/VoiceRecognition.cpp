f8a58f9f0c76eb4a160cd36328c55e549df7e91e
serl <serl@Station5>
2017-06-15 21:16:51 -0700

    Voce is now a standalone node. Voce library inclusion is now automated Enviroment variables VOCE_DIR and JVM_DIR must be set to ensure proper operation

--- src/VoiceRecognition.cpp
+++ src/VoiceRecognition.cpp
@@ -11,88 +11,95 @@
  * Created on November 29, 2016, 8:16 PM
  */
 
-#include <voice_recognition/VoiceRecognition.h>
-#include <voice_recognition/CommandParser.h>
-#include <iostream>
-#include <voce/voce.h>
 #include <boost/algorithm/string.hpp>
-#include <QTimer>
-#include <QThread>
+#include <cstdlib>
+#include <iostream>
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <string>
+#include <voce.h>
+#include <voice_recognition/VoiceRecognition.h>
 
-VoiceRecognition::VoiceRecognition() {
+VoiceRecognition::VoiceRecognition() 
+{
 
 
-//    timer = new QTimer(this);
-//    connect(timer, &QTimer::timeout, this, &VoiceRecognition::run);
-//    timer->start(40);
 
 }
 
-
-VoiceRecognition::~VoiceRecognition() {
+VoiceRecognition::~VoiceRecognition() 
+{
     voce::destroy();
 }
 
-void VoiceRecognition::run()
+void VoiceRecognition::run() 
 {
+    //Nodehandle and publisher
+    ros::NodeHandle nh;
+    ros::Publisher pub = nh.advertise<std_msgs::String>("voice_recognition", 1000);
+
+    //Getting path for voce init
     std::string path = ros::package::getPath("voice_recognition");
     path.append("/resources");
     std::cout << path << std::endl;
 
-    voce::init("/opt/voce/lib", false, true, path, "islurp");
+    //Get Voce library path
+    //Uses "VOCE_DIR" enviromental path
+    std::string voce_lib = std::getenv("VOCE_DIR");
+    voce_lib.append("/lib");
+
+    voce::init(voce_lib, false, true, path, "islurp");
 
     std::cout << "Voice initialize" << std::endl;
 
 
-    std::cout << "running" << std::endl;
-    while (!quit)
+    std::cout << "R unning" << std::endl;
+    
+    bool quit = false;
+    bool started = false;
+    
+    while (!quit) 
     {
-        // Normally, applications would do application-specific things
-        // here.  For this sample, we'll just sleep for a little bit.
 
-        while (voce::getRecognizerQueueSize() > 0)
+        while (voce::getRecognizerQueueSize() > 0) 
         {
             std::string s = voce::popRecognizedString();
 
             // Check if the string contains 'quit'.
-            if (std::string::npos != s.rfind("quit"))
+            if (std::string::npos != s.rfind("quit")) 
             {
                 quit = true;
-                this->thread()->requestInterruption();
                 return;
             }
-            if (s == "end voice")
+
+            if (s == "end voice") 
             {
                 started = false;
-                std::cout << "Voice Recognition stopped!"<< std::endl;
+                std::cout << "Voice Recognition stopped!" << std::endl;
                 break;
-            }
-            else if (s == "start")
+
+            } else if (s == "start") 
             {
-                std::cout << "Voice Recognition started!"<< std::endl;
+                std::cout << "Voice Recognition started!" << std::endl;
                 started = true;
-            }
-            else if (started)
+
+            } else if (started) 
             {
-                 boost::to_upper(s);
+                
+                //Set to uppercase for publishing
+                boost::to_upper(s);
+                std::cout << "TEST: " << s << std::endl;
 
-                 std::cout << "TEST: " << s << std::endl;
-
-                 CommandParser p;
-                 p.parse(s);
-
+                //Create and publish recognized String to "voice_recognition_output"
+                std_msgs::String msg = s;
+                pub.publish(msg);
 
                 std::cout << "You said ----------: " << s << std::endl;
 
+            }
 
-            //voce::synthesize(s);
+        }
 
-            } // else if (started)
-
-        }//while (voce::getRecognizerQueueSize() > 0
-
-        // QCoreApplication::processEvents();
-
-      }  //while(!quit)
+    }
 
 }
